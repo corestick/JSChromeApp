e14db1d30b8d3e64339861eedb219d3a86e003f1
corestick <corestick@gmail.com>
2021-12-26 21:56:59 +0900

    css 수정

--- js/clock.js
+++ js/clock.js
@@ -3,11 +3,11 @@ const clock = document.querySelector("h2#clock");
 function getClock() {
 	const date = new Date();
 
-	const hours = date.getHours().toString().padStart(2, "0");
-	const minutes = date.getMinutes().toString().padStart(2, "0");
-	const seconds = date.getSeconds().toString().padStart(2, "0");
+	//const hours = date.getHours().toString().padStart(2, "0");
+	//const minutes = date.getMinutes().toString().padStart(2, "0");
+	//const seconds = date.getSeconds().toString().padStart(2, "0");
 
-	clock.innerText = `${hours}:${minutes}:${seconds}`;
+	clock.innerText = `${date.toLocaleString()}`;
 }
 
 getClock();
